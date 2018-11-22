a2e03251931c4877925725909c9f8cff76ac1d2e
Anton Aleksandrov <antonaleksandrov24@gmail.com>
2018-11-22 16:22:17 +0100

    Have edited setup script

--- setup.py
+++ setup.py
@@ -1,43 +1,50 @@
-#!/usr/bin/env python
-# coding=utf-8
-
 from setuptools import setup
 
-
-package_name = 'redis-tool'
-filename = 'redis-tool.py'
-
-
-def get_version():
-    import ast
-
-    with open(filename) as input_file:
-        for line in input_file:
-            if line.startswith('__version__'):
-                return ast.parse(line).body[0].value.s
-
-
-def get_long_description():
-    try:
-        with open('README.md', 'r') as f:
-            return f.read()
-    except IOError:
-        return ''
-
-
 setup(
-    name=package_name,
-    version=get_version(),
-    author='AntonAleksandrov',
+    name='python-redis-tool',
+    version='v1.1',
+    packages=['venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.idna',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.pytoml',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.certifi',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.chardet',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.chardet.cli',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.distlib',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.distlib._backport',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.msgpack',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.util',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.contrib',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.contrib._securetransport',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.packages',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.packages.backports',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.packages.ssl_match_hostname',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.colorama',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib._trie',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib.filters',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib.treewalkers',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib.treeadapters',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib.treebuilders',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.lockfile',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.progress',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.requests',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.packaging',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.cachecontrol',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.cachecontrol.caches',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.webencodings',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.pkg_resources',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.req',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.vcs',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.utils',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.models',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.commands',
+              'venv.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.operations', 'model', 'tools'],
+    url='https://github.com/AntonAleksandrov13/python-redis-tool.git',
+    license='',
+    author='Anton Aleksandrov',
     author_email='antonaleksandrov24@gmail.com',
-    description='redis-cluster administration made easy',
-    url='https://github.com/AntonAleksandrov13/python-redis-tool',
-    long_description=get_long_description(),
-    py_modules=[package_name],
-    entry_points={
-        'console_scripts': [
-            'redis-tool= redis-tool:main'
-        ]
-    },
-    license='License :: OSI Approved :: MIT License',
+    description=''
 )
